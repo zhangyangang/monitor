7c15e24d53081f33b92a68a675124d2f7d63debb
elmar-haussmann <elmar-haussmann@gmx.net>
2017-08-11 16:51:16 +0200

    FIx

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DRIVER_DIR=/var/lib/nvidia-docker/volumes/nvidia_driver/latest
 MNT_DIR=/host
 MNT_DRIVER_DIR=${MNT_DIR}${DRIVER_DIR}
