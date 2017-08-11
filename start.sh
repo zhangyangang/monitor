03b60a882176c8dce14329f953c424dc5221b833
Ubuntu <ubuntu@ip-172-21-22-134.us-west-2.compute.internal>
2017-08-11 09:23:01 +0000

    Fix

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DRIVER_DIR=/var/lib/nvidia-docker/volumes/nvidia_driver/latest
+MNT_DIR=/host
+MNT_DRIVER_DIR=${MNT_DIR}${DRIVER_DIR}
+DRIVER_TARGET=/usr/local/nvidia
+
+# DRIVER_DIR is a symlink
+if [ -L $MNT_DRIVER_DIR ]; then
+    TARGET=$(readlink $MNT_DRIVER_DIR)
+    MNT_TARGET=${MNT_DIR}${TARGET}
+    echo Found driver in $MNT_TARGET
+    ln -s $MNT_TARGET $DRIVER_TARGET
+else
+   echo No driver found
+fi
+python3 ${CURRENT_DIR}/monitor.py
