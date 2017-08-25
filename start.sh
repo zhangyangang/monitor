8e1afd63f31bc3ed1d122eb52ff3f7163c4cbad1
elmarhaussmann <elmar@riseml.com>
2017-08-25 18:16:30 +0800

    Fix driver discovery

--- start.sh
+++ start.sh
@@ -7,11 +7,17 @@ DRIVER_TARGET=/usr/local/nvidia
 
 # DRIVER_DIR is a symlink
 if [ -L $MNT_DRIVER_DIR ]; then
-    DRIVER=$(cd $MNT_DRIVER_DIR; pwd -P)
+    LINK_TARGET=$(readlink $MNT_DRIVER_DIR)
+    if [[ ${LINK_TARGET:0:1} = / ]]; then
+        DRIVER=$(realpath $MNT_DIR/$LINK_TARGET)
+    else
+        DRIVER=$(realpath $MNT_DRIVER_DIR)
+    fi
+    #DRIVER=$(cd $MNT_DRIVER_DIR; pwd -P)
     echo Found driver in $DRIVER
     ln -s $DRIVER $DRIVER_TARGET
 else
-   echo No driver found
+   echo No driver found. $NVIDIA_DRIVER_DIR must be a link.
 fi
 
 export PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}
